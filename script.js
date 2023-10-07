64a7faff4c61d8213cdf6b3e8daf4ed024a89097
Kadir Eskituerk <abdulkadir256@hotmail.com>
2023-10-07 20:41:36 +0200

    Zeitstrahl

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-/*
-document.getElementById('arrow1').addEventListener('click', function() {
-    document.getElementById('milestone1').scrollIntoView({ behavior: 'smooth' });
+document.getElementById("arrow1").addEventListener("click", function () {
+  document.getElementById("milestone1").scrollIntoView({ behavior: "smooth" });
 });
 
-document.getElementById('arrow2').addEventListener('click', function() {
-    document.getElementById('milestone2').scrollIntoView({ behavior: 'smooth' });
+document.getElementById("arrow2").addEventListener("click", function () {
+  document.getElementById("milestone2").scrollIntoView({ behavior: "smooth" });
 });
 
-document.getElementById('arrow3').addEventListener('click', function() {
-    document.getElementById('milestone3').scrollIntoView({ behavior: 'smooth' });
+document.getElementById("arrow3").addEventListener("click", function () {
+  document.getElementById("milestone3").scrollIntoView({ behavior: "smooth" });
 });
 
-document.getElementById('arrow4').addEventListener('click', function() {
-    document.getElementById('milestone4').scrollIntoView({ behavior: 'smooth' });
+document.getElementById("arrow4").addEventListener("click", function () {
+  document.getElementById("milestone4").scrollIntoView({ behavior: "smooth" });
 });
-*/
+
 document.addEventListener("DOMContentLoaded", function () {
   const timelineContainer = document.querySelector(".timeline-container");
   const timeline = document.querySelector(".timeline");
