7b5dc42e37a2102e652d569dc363017c70ed25f1
Kadir Eskituerk <abdulkadir256@hotmail.com>
2023-10-07 19:14:45 +0200

    Update Zeitstrahl

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 document.getElementById('arrow1').addEventListener('click', function() {
     document.getElementById('milestone1').scrollIntoView({ behavior: 'smooth' });
 });
@@ -12,4 +13,14 @@ document.getElementById('arrow3').addEventListener('click', function() {
 
 document.getElementById('arrow4').addEventListener('click', function() {
     document.getElementById('milestone4').scrollIntoView({ behavior: 'smooth' });
+});
+*/
+document.addEventListener("DOMContentLoaded", function () {
+  const timelineContainer = document.querySelector(".timeline-container");
+  const timeline = document.querySelector(".timeline");
+
+  timelineContainer.addEventListener("scroll", () => {
+    const scrollLeft = timelineContainer.scrollLeft;
+    timeline.style.transform = `translateX(-${scrollLeft}px)`;
+  });
 });
